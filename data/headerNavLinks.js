7575fd5bb3e098585aeb1bf88dfa5482109b4270
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-03-17 18:00:25 +0530

    updated data

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,6 +1,6 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
+  // { href: '/blog', title: 'Blog' },
   { href: '/contact', title: 'contact' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
