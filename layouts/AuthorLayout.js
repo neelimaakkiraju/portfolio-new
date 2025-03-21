1f201919591e6d5644d11026a813f34b7e7db110
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-03-21 14:45:44 +0530

    new changes

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -32,6 +32,7 @@ export default function AuthorLayout({ children, frontMatter }) {
         </div>
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
           <div className="flex flex-col items-center space-x-2 pt-8 xl:sticky xl:top-0">
+            <div>{avatar}</div>
             <Image
               src={avatar}
               alt="avatar"
